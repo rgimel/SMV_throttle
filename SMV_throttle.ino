0e594b9f44c0438f08e457cd20ace6ff7a68733f
rgimel <56706235+rgimel@users.noreply.github.com>
2020-08-08 23:00:00 -0700

    Update SMV_throttle.ino

--- SMV_throttle.ino
+++ SMV_throttle.ino
@@ -15,9 +15,14 @@
 
 #define throttleIn 5   // Placeholders
 #define ESCOutput 3
+#define button 7
 
 const int minPulseWidth = 1000; // microseconds
 const int maxPulseWidth = 2000;
+const int fullThrottle = 5;//placeholder, amount of time to get to full throttle
+//have to define pin for button 
+
+const bool isButton = false;
 
 Servo ESC;
 
@@ -29,22 +34,56 @@ void setup() {
 const double throttleMinVoltage = 0.8;
 const double throttleMaxVoltage = 4.2;
 
+bool isPressed = false;
+int pressStart;
+
+
+int mappingFunction(int f, int s){
+  //linear
+  if(0){
+    return min(s/fullThrottle, 1) * 1023;
+  }
+  //exponential
+  if(1){
+  }
+  //logarithmic
+  if(2){
+  }
+}
+
 int readThrottle() {
   /*
    * Reads the throttle input from analog signal,
    * Returns 0~1023
    */
-  int throttleReading = analogRead(throttleIn);
-  int throttleMin = throttleMinVoltage * 1023, throttleMax = throttleMaxVoltage * 1023;
-  // Throttle cutoff threshold
-  if (throttleReading < throttleMin) {
-    return 0;
-  } else if (throttleReading < throttleMax) {
-    return map(throttleReading, throttleMin, throttleMax, 0, 1023);
-  } else {
-    return 1023;
+  if(!isButton){
+    int throttleReading = analogRead(throttleIn);
+    int throttleMin = throttleMinVoltage * 1023, throttleMax = throttleMaxVoltage * 1023;
+    // Throttle cutoff threshold
+    if (throttleReading < throttleMin) {
+      return 0;
+    } else if (throttleReading < throttleMax) {
+      return map(throttleReading, throttleMin, throttleMax, 0, 1023);
+    } else {
+      return 1023;
+    }
+  }
+  else{
+    if(button == HIGH){
+      if(!isPressed){
+        pressStart = millis();
+      }
+      int timePressed = millis() - pressStart;
+      //using a linear mapping function(for now)
+      return mappingFunction(0, timePressed);
+    }
+    else{
+      isPressed = false;
+      return 0;
+    }
   }
 }
+
 
 void writeToESC(int throttleRead) { 
   ESC.write(map(throttleRead, 0, 1023, 0, 180));
